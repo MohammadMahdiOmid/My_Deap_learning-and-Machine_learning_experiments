c4deb3e7fb009c5d8cbc0920d87af47833b41e52
MohammadMahdiOmid <m2m@gmail.com>
2020-04-01 00:16:38 +0430

    dataset and regularization are added

---
+++ datasets/simpleDatasetLoader.py
@@ -0,0 +1,48 @@
+import cv2
+import numpy as np
+import os
+
+
+class SimpleDatasetLoader:
+
+    def __init__(self, preprocessor=None):
+        # store the image preprocessors
+        self.preprocessor = preprocessor
+
+        # if the preprocessors are None, initialize them as an
+        # empty list
+        if preprocessor is None:
+            preprocessor = []
+
+    def load(self, imagePaths, verbose=-1):
+        # initialize the list of features and labels
+
+        data = []
+        labels = []
+
+        # loop over the input images
+        for (i, imagePaths) in enumerate(imagePaths):
+            # load the image and extract the class label assuming
+            # that our path has the following format:
+            # /path/to/dataset/{class}/{image}.jpg
+
+            image = cv2.imread(imagePaths)
+            label = imagePaths.split(os.path.sep)[-2]
+
+            # check to see if our preprocessors are not None
+            if self.preprocessor is not None:
+                # loop over the preprocessors and apply each to
+                # the image
+                for p in self.preprocessor:
+                    image = p.preprocess(image)
+
+            # treat our processed image as a "feature vector"
+            # by updating the data list followed by the labels
+            data.append(image)
+            labels.append(label)
+
+            # show an update every ‘verbose‘ images
+            if verbose > 0 and i > 0 and (i + 1) % verbose == 0:
+                print("[INFO] processed {}/{}".format(i + 1, len(imagePaths)))
+
+        return np.array(data), np.array(labels)
